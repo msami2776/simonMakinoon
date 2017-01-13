cac303a5274fe978364d6f923c025ead88ad5e8b
Administrator <Administrator@HQDN--20101219V>
2017-01-13 07:57:20 -0500

    757

--- SimonMakinoon/src/partnerCodeInHerePlease/ButtonAnna.java
+++ SimonMakinoon/src/partnerCodeInHerePlease/ButtonAnna.java
@@ -3,6 +3,8 @@ package partnerCodeInHerePlease;
 import java.awt.Color;
 
 import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.RenderingHints;
 
 import gui.components.Action;
 import gui.components.Component;
@@ -10,9 +12,16 @@ import simonMakinoon.ButtonInterfaceMakinoon;
 
 public class ButtonAnna extends Component implements ButtonInterfaceMakinoon {
 
+	private Action action;
+	private Color displayColor;
+	private boolean highlight;
+	private Color c;
+	private String name;
+	private static final int w = 50;
+	private static final int h = 50;
+
 	public ButtonAnna() {
-		super(0,0,0,0);
-		//change the numbers inside super
+		super(0,0,w,h);
 		
 	}
 
@@ -20,25 +29,30 @@ public class ButtonAnna extends Component implements ButtonInterfaceMakinoon {
 	@Override
 	public void act() {
 		// TODO Auto-generated method stub
-
+		action.act();
 	}
 
 	@Override
-	public boolean isHovered(int arg0, int arg1) {
+	public boolean isHovered(int i, int j) {
 		// TODO Auto-generated method stub
-		return false;
+		double distance = Math.sqrt(Math.pow(i-(getX()+w/2), 2) + Math.pow(j-(getY()+h/2), 2));
+		return distance < w/2;
 	}
 
 	@Override
 	public void highlight() {
 		// TODO Auto-generated method stub
-
+		if(c != null) displayColor = c;
+		highlight = true;
+		update();
 	}
 
 	@Override
 	public void setColor(Color color) {
 		// TODO Auto-generated method stub
-
+		this.c = color;
+		displayColor = c;
+		update();
 	}
 
 	@Override
@@ -56,25 +70,50 @@ public class ButtonAnna extends Component implements ButtonInterfaceMakinoon {
 	@Override
 	public void setAction(Action action) {
 		// TODO Auto-generated method stub
-
+		this.action = action;
 	}
 
 	@Override
 	public void dim() {
 		// TODO Auto-generated method stub
-
+		displayColor = Color.black;
+		highlight = false;
+		update();
 	}
 
 	@Override
-	public void update(Graphics2D arg0) {
+	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
-
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		if(displayColor != null){ 
+			g.setColor(displayColor);
+		}
+		else{ 
+			g.setColor(Color.gray);
+		}
+		g.fillOval(0, 0, w, h);
+		g.setColor(Color.black);
+		g.drawOval(0, 0, w-1, h-1);
+		if(highlight){
+			g.setColor(Color.white);
+			Polygon p = new Polygon();
+			
+			int s = (int)(5/8.0 * w);
+			int t = (int)(1.0/5*h)+4;
+			p.addPoint(s-4, t-4);
+			p.addPoint(s+7, t-2);
+			p.addPoint(s+10, t);
+			p.addPoint(s+14, t+10);
+			p.addPoint(s+12, t+14);
+			p.addPoint(s+8, t+3);
+			g.fill(p);
+		}
 	}
 
 	@Override
 	public void setName(String name) {
 		// TODO Auto-generated method stub
-		
+		this.name = name;
 	}
 
 }
--- SimonMakinoon/src/partnerCodeInHerePlease/ProgressAnna.java
+++ SimonMakinoon/src/partnerCodeInHerePlease/ProgressAnna.java
@@ -1,39 +1,68 @@
 package partnerCodeInHerePlease;
 
+import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import gui.components.Component;
 import simonMakinoon.ProgressInterfaceMakinoon;
 
 public class ProgressAnna extends Component implements ProgressInterfaceMakinoon {
 
+	private String round;
+	private String sequence;
+	private boolean gameOver;
+
 	public ProgressAnna() {
-		super(0,0,0,0);
-		//change up the above numbers as you can
+		super(60,60,100,50);
 	}
 
 	@Override
 	public void setRound(int roundNumber) {
 		// TODO Auto-generated method stub
-
+		round = "Round "+roundNumber;
+		update();
 	}
 
 	@Override
 	public void setSequenceLength(int size) {
 		// TODO Auto-generated method stub
-
+		sequence = "Win Streak: "+size;
+		update();
 	}
 
 	@Override
 	public void gameOver() {
 		// TODO Auto-generated method stub
-
+		gameOver = true;
+		update();
 	}
 
 	@Override
-	public void update(Graphics2D arg0) {
+	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		FontMetrics fm = g.getFontMetrics();
+		if(gameOver){
+			g.setColor(Color.blue);
+			g.fillRect(0, 0, 100, 50);
+			g.setColor(Color.white);
+			String go = "GAME OVER!";
+			g.drawString(go, (100 - fm.stringWidth(go))/2, 20);
+			g.drawString(sequence, (100 - fm.stringWidth(sequence))/2, 40);
 
+		}else{
+			g.setColor(Color.green);
+			g.fillRect(0, 0, 100, 50);
+			g.setColor(Color.black);
+			g.drawRect(0, 0, 100-1, 50-1);
+			if(round !=null && sequence != null){
+
+				g.drawString(round, (100 - fm.stringWidth(round))/2, 20);
+				g.drawString(sequence, (100 - fm.stringWidth(sequence))/2, 40);
+			}
+		}
 	}
 
 }
